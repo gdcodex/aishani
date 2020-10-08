1109dcaa945aa582171e6ef5312beff2cc1de610
Geremsa <geremsa111@gmail.com>
2020-10-08 21:33:32 +0530

    grid container height constant fixed by adding extra small sized row

---
+++ src/TrialProjects/Libraryy/img.js
@@ -0,0 +1,15 @@
+const images = [
+    { id: 1, src: '/images/badguy.png' },
+    { id: 2, src: '/images/Teen.png' },
+    { id: 3, src: '/images/pains.png' },
+    { id: 4, src: '/images/east.jpg' },
+    { id: 5, src: '/images/inner.jpg' },
+    { id: 6, src: '/images/likemebetter.png' },
+    { id: 7, src: '/images/worldend.png' },
+    { id: 8, src: '/images/closer.jpg' },
+    { id: 9, src: '/images/house.jpg' },
+
+  ];
+
+  
+export default images;
--- src/TrialProjects/Libraryy/libbody.js
+++ src/TrialProjects/Libraryy/libbody.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import images from './img';
 
 
 const Libbody =()=>{
@@ -6,18 +7,18 @@ const Libbody =()=>{
     return(
         <>
              <div className="lib-body">
-                <div className="libra-types" id="lib5"></div>
-                <div className="libra-types" id="lib6"></div>
-                <div className="libra-types" id="lib7"></div>
-                <div className="libra-types" id="lib8"></div>
-                <div className="libra-types" id="lib9"></div>
-                <div className="libra-types" id="lib10"></div>
-                <div className="libra-types" id="lib11"></div>
-                <div className="libra-types" id="lib12"></div>
-                <div className="libra-types" id="lib13"></div>
-                <div className="libra-types" id="lib14"></div>
-                <div className="libra-types" id="lib15"></div>
-                <div className="libra-types" id="lib16"></div>
+                <div className="libra-types" id="lib5"><img src={images[0].src} alt=""/></div>
+                <div className="libra-types" id="lib6">  <img src={images[1].src} alt=""/> </div>
+                <div className="libra-types" id="lib7">  <img src={images[2].src} alt=""/> </div>
+                <div className="libra-types" id="lib8">  <img src={images[3].src} alt=""/> </div>
+                <div className="libra-types" id="lib9">  <img src={images[4].src} alt=""/> </div>
+                <div className="libra-types" id="lib10">  <img src={images[5].src} alt=""/> </div>
+                <div className="libra-types" id="lib11">  <img src={images[6].src} alt=""/> </div>
+                <div className="libra-types" id="lib12">  <img src={images[7].src} alt=""/> </div>
+                <div className="libra-types" id="lib13">  <img src={images[8].src} alt=""/> </div>
+                <div className="libra-types" id="lib14">  <img src={images[1].src} alt=""/> </div>
+                <div className="libra-types" id="lib15">  <img src={images[1].src} alt=""/> </div>
+                <div className="libra-types" id="lib16">  <img src={images[1].src} alt=""/> </div>
                 </div>
         </>
     )
