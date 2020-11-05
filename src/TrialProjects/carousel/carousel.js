19cd2e5d1c774fb32555127a4cb6a27e58da2b83
Geremsa <geremsa111@gmail.com>
2020-11-05 23:21:33 +0530

    ...

--- src/TrialProjects/carousel/carousel.js
+++ src/TrialProjects/carousel/carousel.js
@@ -8,39 +8,31 @@ const Carousel = () => {
     "https://cdn.pixabay.com/photo/2020/10/27/07/40/cheetah-5689870__480.jpg",
     "https://cdn.pixabay.com/photo/2020/10/29/05/02/buddhist-5695220__480.jpg",
     "https://cdn.pixabay.com/photo/2020/10/21/19/43/jack-o-lanterns-5674148__480.jpg",
-    "https://cdn.pixabay.com/photo/2020/05/24/06/54/dumbo-5212670_1280.jpg"
   ];
-  
-  const one =()=>{
-          document.getElementById("slider").className = "carousel-slider one"
-  }
-  const two =()=>{
-          document.getElementById("slider").className = "carousel-slider two"
-  }
-  const three =()=>{
-          document.getElementById("slider").className = "carousel-slider three"
-  }
-  const four =()=>{
-          document.getElementById("slider").className = "carousel-slider four"
-  }
- 
 
   return (
     <>
+    <a href="#jhgd">Hello</a>
       <section className="carousel-container">
-        <div className="carousel-slider one" id="slider">
+        <div className="carousel-slider" id="slider">
           {Irray.map((e, i) => (
-            <img key={"c-" + i} src={e} className="carousel-image" id={"pic" + i}/>
-            
+            <img
+              key={"c-" + i}
+              src={e}
+              className="carousel-image"
+              id={`pic_${i}`}
+            />
           ))}
         </div>
-        <div className="indicator">
-          <span onClick={one}>A</span>
-          <span onClick={two}>B</span>
-          <span onClick={three}>C</span>
-          <span onClick={four}>D</span>
-        </div>
       </section>
+        <div className="indicator">
+          <a href="#pic_0" onClick={()=>{
+
+          }}>A</a>
+          <a href="#pic_1">B</a>
+          <a href="#pic_2">C</a>
+          <a href="#pic_3">D</a>
+        </div>
     </>
   );
 };
