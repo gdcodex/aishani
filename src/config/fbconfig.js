400d06a12187a6606f0e0f9409c8042b5f106e62
Geremsa <geremsa111@gmail.com>
2020-11-27 22:01:12 +0530

    env variables firebase config

--- src/config/fbconfig.js
+++ src/config/fbconfig.js
@@ -7,13 +7,13 @@
  // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
-    apiKey: "AIzaSyAL5xNSVraW1ao75tfdfkqmh2Oo8Kut9YU",
-    authDomain: "aishani-gdai.firebaseapp.com",
-    databaseURL: "https://aishani-gdai.firebaseio.com",
-    projectId: "aishani-gdai",
-    storageBucket: "aishani-gdai.appspot.com",
-    messagingSenderId: "751734523543",
-    appId: "1:751734523543:web:b29b704ad57980a5ac0bd2",
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    databaseURL: process.env.REACT_APP_databaseURL,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
     measurementId: "G-2Z97EV6XS3"
   };
   // Initialize Firebase
